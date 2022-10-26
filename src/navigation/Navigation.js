9944a262bd6df71c94bf6df17790b36fc9826f2e
Francisco Javier Velez Alvarez <fjvelezdev@gmail.com>
2022-10-26 09:55:42 -0500

    cambios en menu

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -3,6 +3,8 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { Image } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome5'
 import ListCoffeNavigation from './ListCoffeNavigation'
+import Home from '../screens/Home'
+import Perfil from '../screens/Perfil'
 
 const Tab = createBottomTabNavigator();
 
@@ -10,11 +12,32 @@ export const Navigation = () => {
   return (
     <Tab.Navigator>
 
+        <Tab.Screen name="home" component={Home} options={
+          {
+            headerShown: false,
+            tabBarLabel: "Home",
+            tabBarIcon: ({color, size}) => (
+              <Icon name="home" color={color} size={size} />
+            )
+          }
+        } />
+
         <Tab.Screen name="ListCoffeNavigation" component={ListCoffeNavigation} options={{
           headerShown: false,
           tabBarLabel: "",
           tabBarIcon: () => renderCoffe()
         }}/>
+
+        <Tab.Screen name="perfil" component={Perfil} options={
+          {
+            headerShown: false,
+            tabBarLabel: "Perfil",
+            tabBarIcon: ({color, size}) => (
+              <Icon name="user" color={color} size={size} />
+            )
+          }
+        } />
+
 
     </Tab.Navigator>
   )
@@ -23,8 +46,8 @@ export const Navigation = () => {
 function renderCoffe(){
   return (
     <Image 
-      source={require('../assets/cafe.png')}
-      style={{width: 75, height: 75, top: -15}}
+      source={require('../assets/la_tiendadelcafeicon.png')}
+      style={{width: 60, height: 60, top: -5, borderRadius: 300}}
     />
   )
 }
---
+++ src/screens/Perfil.js
@@ -0,0 +1,10 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+export default function Perfil() {
+  return (
+    <SafeAreaView>
+      <Text>Perfil</Text>
+    </SafeAreaView>
+  )
+}
